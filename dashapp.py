b0fd12fd35ba0d191027a2cdb4b5c8d422bedba1
MysticPenguin <blue.harrell17@gmail.com>
2026-01-30 10:45:19 -0500

    Dashapp built

--- dashapp.py
+++ dashapp.py
@@ -5,13 +5,9 @@ app = Dash()
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+df = pd.read_csv(".//data//daily_sales_data_clean.csv")
+df = df.sort_values(by="date", ascending=False)
+fig = px.line(df, x="date", y="sold")
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
