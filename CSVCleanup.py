ef512a95da5c0ddda18e043f9812a764a80517d5
MysticPenguin <blue.harrell17@gmail.com>
2026-01-29 22:28:38 -0500

    Update clean files

--- CSVCleanup.py
+++ CSVCleanup.py
@@ -11,7 +11,7 @@ def clean(sr):
         # Convert the clean string to a float
         cost_float = float(cost_str)
         total = cost_float * quant
-        clean_row = ["pink morsel", f"${total:,.2f}",array[3],array[4]]
+        clean_row = [f"${total:,.2f}",array[3],array[4]]
         data_list.append(clean_row)
 
 
@@ -22,7 +22,7 @@ df = pd.read_csv('C:\\Users\\Benja\\Documents\\CSE 495 Project\\quantium-starter
 df.apply(clean, axis=1, raw = False)
 
 
-clean_data = pd.DataFrame(columns=["product","sold","date","region"],data=data_list)
+clean_data = pd.DataFrame(columns=["sold","date","region"],data=data_list)
 print(clean_data)
 clean_data.to_csv("daily_sales_data_clean_2.csv")
 # for i in df.index:
